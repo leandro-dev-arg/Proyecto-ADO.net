213fd1123b339010a816897cbdbd5c21ee2d3bb3
leandro <leandro.dev.arg@gmail.com>
2024-01-25 08:25:04 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Proyecto_ADO.net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPokemons = new System.Windows.Forms.DataGridView();
+            this.PbPokemon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbPokemon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPokemons
+            // 
+            this.dgvPokemons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPokemons.Location = new System.Drawing.Point(34, 23);
+            this.dgvPokemons.Name = "dgvPokemons";
+            this.dgvPokemons.Size = new System.Drawing.Size(398, 274);
+            this.dgvPokemons.TabIndex = 0;
+            this.dgvPokemons.SelectionChanged += new System.EventHandler(this.dgvPokemons_SelectionChanged);
+            // 
+            // PbPokemon
+            // 
+            this.PbPokemon.Location = new System.Drawing.Point(491, 23);
+            this.PbPokemon.Name = "PbPokemon";
+            this.PbPokemon.Size = new System.Drawing.Size(281, 274);
+            this.PbPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbPokemon.TabIndex = 1;
+            this.PbPokemon.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 385);
+            this.Controls.Add(this.PbPokemon);
+            this.Controls.Add(this.dgvPokemons);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbPokemon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPokemons;
+        private System.Windows.Forms.PictureBox PbPokemon;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominios;
+
+namespace Proyecto_ADO.net
+{
+    public partial class Form1 : Form
+    {
+       private List<Pokemon> listaPokemon;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio= new PokemonNegocio();
+            listaPokemon= negocio.listar();
+            dgvPokemons.DataSource = listaPokemon;
+            dgvPokemons.Columns["UrlImagen"].Visible=false;
+            cargarImagen(listaPokemon[0].UrlImagen);
+        }
+
+        private void dgvPokemons_SelectionChanged(object sender, EventArgs e)
+        {
+           Pokemon Seleccionado= (Pokemon)dgvPokemons.CurrentRow.DataBoundItem;
+            cargarImagen(Seleccionado.UrlImagen);
+        }
+
+        private void cargarImagen(string imagen) {
+
+            try
+            {
+                PbPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                PbPokemon.Load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuqKNTAK2agAe2tqc7_uKuIIEG74Ydj4FMrw&usqp=CAU");
+            }
+
+
+        }
+
+    }
+}
